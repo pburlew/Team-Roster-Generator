5ebfab7024fb9b25333b5b34ee0058f258dc885a
Paige Burlew <paigeburlew@gmail.com>
2020-10-10 13:10:48 -0500

    manager html

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,16 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+var Employee = require("./Employee");
+
+class Manager extends Employee{
+
+    constructor(officeNumber){
+        this.officeNumber = officeNumber;
+    }
+
+    getRole(){
+        return "Manager";
+    }
+
+}
+
+module.exports = {manager : Manager}
