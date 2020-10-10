d62cba362fd8e328f7863c7c659d775dfa1b426b
Paige Burlew <paigeburlew@gmail.com>
2020-10-10 13:08:12 -0500

    updated htmls

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+ 
+var Employee = require("./Employee");
+
+class Engineer extends Employee{
+
+    constructor(github){
+        this.github = github;
+    }
+
+    getGithub(){
+        return this.github;
+    }
+
+    getRole(){
+        return "Engineer";
+    }
+
+
+}
+
+module.exports = {engineer : Engineer}
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+var Employee = require("./Employee");
+
+class Intern extends Employee {
+
+    constructor(school){
+        this.school = school;
+    }
+
+    getSchool(){
+        return this.school;
+    }
+
+    getRole(){
+        return "Intern";
+
+    }
+}
+
+module.exports = {intern : Intern}
